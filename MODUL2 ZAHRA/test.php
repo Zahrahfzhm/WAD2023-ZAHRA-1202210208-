82eda87da1f6edef90c462963c8cc5bdc5bf8144
Zahrahfzhm <zahrahafizhahm@gmail.com>
2023-10-24 08:07:07 +0700

    modul 2

---
+++ MODUL2 ZAHRA/test.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" contont="width=device-width, initial-scale=1.0">
+	<title>Test</title>
+</head>
+<body>
+	<p>Hello World</p>
+</body>
+</html>
